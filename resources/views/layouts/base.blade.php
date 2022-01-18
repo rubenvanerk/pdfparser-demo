1916164584b9d06cae8deb2f89b08f44d386f20a
Ruben van Erk <ruben@rubenvanerk.nl>
2022-01-18 19:58:33 +0100

    Add footer & upgrade tailwind

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -28,7 +28,7 @@
 </head>
 
 <body class="h-full">
-<div class="min-h-full">
+<div class="min-h-full flex flex-col">
     <nav class="bg-white shadow-sm">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-16">
@@ -42,9 +42,11 @@
                         <a href="https://github.com/smalot/pdfparser"
                            target="_blank"
                            class="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium">
-                            GitHub
-                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                            PdfParser on GitHub
+                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-3 w-3" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/>
                             </svg>
                         </a>
                     </div>
@@ -53,13 +55,37 @@
         </div>
     </nav>
 
-    <div class="py-10">
-        <main>
-            <div class="max-w-3xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
-                @yield('body')
+    <div class="flex flex-col justify-between grow">
+        <div class="py-10">
+            <main>
+                <div class="max-w-3xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8">
+                    @yield('body')
+                </div>
+            </main>
+        </div>
+
+        <footer class="bg-white">
+            <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+                <div class="flex justify-center space-x-6 md:order-2">
+                    <a href="https://github.com/rubenvanerk/pdfparser-demo" class="text-gray-400 hover:text-gray-500"
+                       target="_blank">
+                        <span class="sr-only">GitHub</span>
+                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path fill-rule="evenodd"
+                                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                                  clip-rule="evenodd"/>
+                        </svg>
+                    </a>
+                </div>
+                <div class="mt-8 md:mt-0 md:order-1">
+                    <p class="text-center text-base text-gray-400">
+                        smalot/pdfparser version: <code>dev-master#ddb14aedf47c0e9848dc6c183d3bb6f3cdb7f9d5</code>
+                    </p>
+                </div>
             </div>
-        </main>
+        </footer>
     </div>
+
 </div>
 
 @livewireScripts
