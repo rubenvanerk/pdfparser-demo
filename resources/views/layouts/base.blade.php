29bc9abb39ae867534dd27938a3f0bb7bdcafd41
Ruben van Erk <github@accounts.rubenvanerk.nl>
2022-02-03 21:38:21 +0100

    SHow pdfparser version on top on small screens

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -67,7 +67,12 @@
 
         <footer class="bg-white">
             <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-                <div class="flex justify-center space-x-6 md:order-2">
+                <div class="mb-8 md:mb-0">
+                    <p class="text-center text-base text-gray-400">
+                        smalot/pdfparser version: <code><a href="https://github.com/smalot/pdfparser/releases/tag/v2.1.0" class="hover:text-gray-500" target="_blank">v2.1.0</a></code>
+                    </p>
+                </div>
+                <div class="flex justify-center space-x-6">
                     <a href="https://plausible.wrve.nl/pdfparser.wrve.nl" target="_blank" class="text-gray-400 hover:text-gray-500">
                         User statistics
                     </a>
@@ -80,11 +85,6 @@
                                   clip-rule="evenodd"/>
                         </svg>
                     </a>
-                </div>
-                <div class="mt-8 md:mt-0 md:order-1">
-                    <p class="text-center text-base text-gray-400">
-                        smalot/pdfparser version: <code><a href="https://github.com/smalot/pdfparser/releases/tag/v2.1.0" class="hover:text-gray-500" target="_blank">v2.1.0</a></code>
-                    </p>
                 </div>
             </div>
         </footer>
